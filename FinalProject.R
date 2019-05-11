2aec2eb6defb6eff9ad921743e18c19c2854d1e7
Wenjie Zheng <40931861+wz1405@users.noreply.github.com>
2019-05-11 16:07:47 -0400

    Update FinalProject.R

--- FinalProject.R
+++ FinalProject.R
@@ -6,7 +6,7 @@ libraries <- c("ldatuning", "topicmodels", "ggplot2",
                "tidyr", "xtable", "devtools", "utf8", "preText",
                "gutenbergr", "data.table", "stringi", "stringr",
                "xml2", "rvest", "tidyverse", "reshape2","httr",
-               "ROAuth", "twitteR")
+               "ROAuth", "twitteR", "readtext")
 lapply(libraries, require, character.only = TRUE)
 
 setwd("/Users/zhengwenjie/Documents/RData/") 
@@ -15,26 +15,6 @@ setwd("/Users/zhengwenjie/Documents/RData/")
 
 
 # read in the csv as of 421, 428, 510
-MTA_Timeline_DF_421 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf421/mta_timeline_df.csv", header=TRUE, sep=",", stringsAsFactors = F)
-MTA_Timeline_DF_428 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf428/mta_timeline_df_428.csv", header=TRUE, sep=",", stringsAsFactors = F)
-MTA_Timeline_DF_510 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf510/mta_timeline_df_510.csv", header=TRUE, sep=",", stringsAsFactors = F)
-glimpse(MTA_Timeline_DF_421)
-
-NYCTSubway_Timeline_DF_421 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf421/NYCTSubway_timeline_df.csv", header=TRUE, sep=",", stringsAsFactors = F)
-NYCTSubway_Timeline_DF_428 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf428/NYCTSubway_timeline_df_428.csv", header=TRUE, sep=",", stringsAsFactors = F)
-NYCTSubway_Timeline_DF_510 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf510/NYCTSubway_timeline_df_510.csv", header=TRUE, sep=",", stringsAsFactors = F)
-nrow(NYCTSubway_Timeline_DF_421)
-nrow(NYCTSubway_Timeline_DF_428)
-nrow(NYCTSubway_Timeline_DF_510)
-glimpse(NYCTSubway_Timeline_DF_421)
-
-NYCTBus_Timeline_DF_421 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf421/NYCTBus_timeline_df.csv", header=TRUE, sep=",", stringsAsFactors = F)
-NYCTBus_Timeline_DF_428 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf428/NYCTBus_timeline_df_428.csv", header=TRUE, sep=",", stringsAsFactors = F)
-NYCTBus_Timeline_DF_510 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf510/NYCTBus_timeline_df_510.csv", header=TRUE, sep=",", stringsAsFactors = F)
-nrow(NYCTBus_Timeline_DF_421)
-nrow(NYCTBus_Timeline_DF_428)
-nrow(NYCTBus_Timeline_DF_510)
-
 MTA_Hashtags_DF_421 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf421/mta_hashtags.csv", header=TRUE, sep=",", stringsAsFactors = F)
 MTA_Hashtags_DF_428 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf428/tweetsDF_428.csv", header=TRUE, sep=",", stringsAsFactors = F)
 MTA_Hashtags_DF_510 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf510/tweetsDF_510.csv", header=TRUE, sep=",", stringsAsFactors = F)
@@ -55,58 +35,46 @@ mta_hashtags_merged <- mta_hashtags_merged[mta_hashtag_columns]
 # drop duplicated & overlapped data for some dates
 # not working: mta_hashtags_merged_drop <- mta_hashtags_merged[!duplicated(mta_hashtags_merged$text), ]
 mta_hashtags_merged <- mta_hashtags_merged[!(duplicated(mta_hashtags_merged[c("text","created")]) | duplicated(mta_hashtags_merged[c("text","created")], fromLast = F)), ]
+# get rid of @mta_mood, posts affect the data and possibly results
+mta_hashtags_merged <- mta_hashtags_merged[!(mta_hashtags_merged$screenName== 'mta_mood'),]
 
+# sentiment analysis
+# Get the data directory from readtable
+positivewords <- read.table("/Users/zhengwenjie/Documents/RData/positive-words.txt")
+negativewords <- read.table("/Users/zhengwenjie/Documents/RData/negative-words.txt")
+# unlist and as.character make them bag of words
+positivewords <- as.character(unlist(positivewords))
+negativewords <- as.character(unlist(negativewords))
 
+mta_hashtags_merged$text <- tolower(mta_hashtags_merged$text)
+mta_hashtags_merged$text <- as.character(mta_hashtags_merged$text)
 
+# calculate and report the occurence of postive & negative words in the review text
+# subtract one with another to get the score
+mta_hashtags_merged$text_positive <- rowSums(dfm(mta_hashtags_merged$text, select = positivewords))
+mta_hashtags_merged$text_negative <- rowSums(dfm(mta_hashtags_merged$text, select = negativewords))
+mta_hashtags_merged$sentiment_score <- mta_hashtags_merged$text_positive - mta_hashtags_merged$text_negative
 
+# if is 0 and above, tag it positive and negative for less than 0
+mta_hashtags_merged$sentiment_attitude[mta_hashtags_merged$sentiment_score > -1] <- 'positive'
+mta_hashtags_merged$sentiment_attitude[mta_hashtags_merged$sentiment_score < 0] <- 'negative'
 
-#ggplot(aes(x = created, y = favoriteCount), data = MTA_Hashtags_DF_510) + geom_point()
+# number of occurence of positive & negative & the total row number in the column
+pos_number <- nrow(mta_hashtags_merged[mta_hashtags_merged$sentiment_score > -1,])
+neg_number <- nrow(mta_hashtags_merged[mta_hashtags_merged$sentiment_score < 0,])
+total_rows <- nrow(mta_hashtags_merged)
 
-
-# 
-
-
-# Summary info 
-colnames(MTA_Timeline_DF)  # column names
-colnames(NYCTSubway_Timeline_DF)
-colnames(NYCTBus_Timeline_DF)
-colnames(MTA_Hashtags_DF)  # column names
-# column names for timeline and hashtags are the same in twitter
-  # dimmension
-dim(MTA_Timeline_DF)  # data dimensions
-dim(NYCTSubway_Timeline_DF)
-dim(NYCTBus_Timeline_DF)
-dim(MTA_Hashtags_DF)
-
-sapply(MTA_Timeline_DF, class) # returns class for each variable (column)
-
-
-
-head(MTA_Timeline_DF)  # display first lines of an object
-head(polling_data, n = 10)  # same as above but specifying number of lines 
-tail(polling_data)  # display last lines of an object
-dim(MTA_Timeline_DF)  # data dimensions
-nrow(polling_data)  # number of rows
-ncol(polling_data)  # number of columns
-colnames(MTA_Timeline_DF)  # column names
-names(polling_data)  # also column names (more general command)
-rownames(polling_data) # row names
-class(polling_data)  # returns class of an R object
-sapply(polling_data, class) # returns class for each variable (column)
-str(polling_data)  # display structure of an R object (e.g. a dataframe)
-glimpse(polling_data)
-
-#userTimeline(user, n=20, maxID=NULL, sinceID=NULL, includeRts=FALSE,
-#             excludeReplies=FALSE, ...)
-#homeTimeline(n=25, maxID=NULL, sinceID=NULL, ...)
-#mentions(n=25, maxID=NULL, sinceID=NULL, ...)
-#retweetsOfMe(n=25, maxID=NULL, sinceID=NULL, ...)
-#Warning message:
-#  In statusBase(cmd, params, n, 3200, ...) :
-#  statuses/user_timeline has a cap of 3200 statuses, clipping
-
-
-
-
-
-
+# calculate and report the ratio
+positive_ratio <- pos_number / total_rows
+negative_ratio <- neg_number / total_rows
+cat(
+  "positive_ratio: ", positive_ratio, "\n",
+  "negative_ratio:",  negative_ratio, "\n"
+)
+# histogram
+hist(mta_hashtags_merged$sentiment_score,
+     main="Histogram of Sentiment Score", 
+     xlab="Sentiment Score",
+     ylab="Histogram Frequency Distribution",
+     border="black", 
+     col="green")
