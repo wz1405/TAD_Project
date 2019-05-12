fa0161c13920ebdade8f561f0dac713e2d7ae74a
Wenjie Zheng <40931861+wz1405@users.noreply.github.com>
2019-05-12 18:42:18 -0400

    Update FinalProject.R

--- FinalProject.R
+++ FinalProject.R
@@ -37,6 +37,9 @@ mta_hashtags_merged <- mta_hashtags_merged[!(duplicated(mta_hashtags_merged[c("t
 # get rid of @mta_mood, posts affect the data and possibly results
 mta_hashtags_merged <- mta_hashtags_merged[!(mta_hashtags_merged$screenName== 'mta_mood'),]
 
+# bigrams
+head(textstat_collocations(mta_hashtags_merged$text))
+textstat_collocations(mta_hashtags_merged$text) %>% arrange(-lambda) %>% slice(1:10)
 
 # preprocessing & cleaning
 mta_hashtags_merged$text <- tolower(mta_hashtags_merged$text)
@@ -176,6 +179,187 @@ ggplot(data=Sentimentscores_mta,aes(x=sentiment,y=Score))+geom_bar(aes(fill=sent
   theme(legend.position="none")+
   xlab("Sentiments")+ylab("scores")+ggtitle("Sentiments of people behind the tweets on MTA hashtags")+ theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+
+
+
+
+
+
+# cta_hashtags
+rm(list = ls())
+
+libraries <- c("ldatuning", "topicmodels", "ggplot2", 
+               "dplyr", "rjson", "quanteda", "lubridate", 
+               "parallel", "doParallel", "tidytext", "stringi", 
+               "tidyr", "xtable", "devtools", "utf8", "preText",
+               "gutenbergr", "data.table", "stringi", "stringr",
+               "xml2", "rvest", "tidyverse", "reshape2","httr",
+               "ROAuth", "twitteR", "readtext", "tm", "SnowballC",
+               "wordcloud", "RColorBrewer", "syuzhet")
+lapply(libraries, require, character.only = TRUE)
+
+setwd("/Users/zhengwenjie/Documents/RData/") 
+
+# read in the csv as of 421, 428, 510
+CTA_Hashtags_DF_421 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf421/tweets_cta.csv", header=TRUE, sep=",", stringsAsFactors = F)
+CTA_Hashtags_DF_428 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf428/tweets_cta_428.csv", header=TRUE, sep=",", stringsAsFactors = F)
+CTA_Hashtags_DF_510 <- read.csv(file="/Users/zhengwenjie/Documents/RData/AsOf510/tweets_cta_510.csv", header=TRUE, sep=",", stringsAsFactors = F)
+nrow(CTA_Hashtags_DF_421)
+nrow(CTA_Hashtags_DF_428)
+nrow(CTA_Hashtags_DF_510)
+
+
+# merge data frames vertically with rbind()
+cta_hashtags_merged <- rbind(CTA_Hashtags_DF_421,CTA_Hashtags_DF_428,CTA_Hashtags_DF_510)
+# sort by date time
+cta_hashtags_merged <- cta_hashtags_merged[order(cta_hashtags_merged$created , decreasing = TRUE ),]
+dim(cta_hashtags_merged)
+# subset
+cta_hashtag_columns <- c("X", "text", "favoriteCount", "created", 
+                         "screenName", "retweetCount", "longitude", "latitude")
+cta_hashtags_merged <- cta_hashtags_merged[cta_hashtag_columns]
+
+# drop duplicated & overlapped data for some dates
+# not working: mta_hashtags_merged_drop <- mta_hashtags_merged[!duplicated(mta_hashtags_merged$text), ]
+cta_hashtags_merged <- cta_hashtags_merged[!(duplicated(cta_hashtags_merged[c("text","created")]) | duplicated(cta_hashtags_merged[c("text","created")], fromLast = F)), ]
+
+# preprocessing & cleaning
+cta_hashtags_merged$text <- tolower(cta_hashtags_merged$text)
+cta_hashtags_merged$text <- removeWords(cta_hashtags_merged$text,stopwords('en'))
+cta_hashtags_merged$text <- removePunctuation(cta_hashtags_merged$text)
+cta_hashtags_merged$text <- gsub('[\r\n]', '', cta_hashtags_merged$text)
+# removing 'rt', remove 'rt : ' characters do not work:
+cta_hashtags_merged$text <- gsub("rt", " ", cta_hashtags_merged$text)
+# remove @ strings: 
+# question: remove @ or not? for now, remove, will better help in analyzing word cloud stuff
+cta_hashtags_merged$text <- gsub("@\\w+", " ", cta_hashtags_merged$text)
+# remove website
+cta_hashtags_merged$text <- gsub("http\\w+", " ", cta_hashtags_merged$text)
+cta_hashtags_merged$text <- gsub("[ |\t]{2,}", " ", cta_hashtags_merged$text)
+cta_hashtags_merged$text <- gsub("^ ", " ", cta_hashtags_merged$text)
+cta_hashtags_merged$text <- gsub(" $", " ", cta_hashtags_merged$text)
+cta_hashtags_merged$text <- gsub("[^\x01-\x7F]", " ", cta_hashtags_merged$text)
+
+# word cloud
+wordcloud(cta_hashtags_merged$text,min.freq = 20,colors=brewer.pal(8, "Dark2"),random.color = TRUE,max.words = 200)
+
+# average retweet and fav
+mean(cta_hashtags_merged$favoriteCount)
+mean(cta_hashtags_merged$retweetCount)
+
+#-----------------------------
+# 4 WEIGHTED DOCUMENT FEATURE MATRIX
+#-----------------------------
+# WHAT ARE WE WEIGHTING?
+
+# Now we will create a DFM of all the SOTU speeches
+cta_hashtag_dfm <- dfm(cta_hashtags_merged$text)
+cta_hashtag_dfm[, 1:10]  # notice sparsity
+topfeatures(cta_hashtag_dfm)
+topfeatures(cta_hashtag_dfm[nrow(cta_hashtag_dfm),])
+
+# 4.1 tfidf - Frequency weighting
+weighted_cta_hashtag_dfm <- dfm_tfidf(cta_hashtag_dfm) # uses the absolute frequency of terms in each document
+topfeatures(weighted_cta_hashtag_dfm)
+topfeatures(weighted_cta_hashtag_dfm[nrow(weighted_cta_hashtag_dfm),])
+
+# 4.2 tfidf - Relative frequency weighting
+?dfm_tfidf
+normalized_cta_hashtag <- dfm_tfidf(cta_hashtag_dfm, scheme_tf = "prop") # Uses feature proportions within documents: divdes each term by the total count of features in the document
+topfeatures(normalized_cta_hashtag)
+topfeatures(normalized_cta_hashtag[nrow(normalized_cta_hashtag),])
+
+# Heap's Law
+tokens <- tokens(cta_hashtags_merged$text) 
+Tee <- sum(lengths(tokens))
+cta_hashtag_dfm <- dfm(cta_hashtags_merged$text)
+M <- nfeat(cta_hashtag_dfm)  # number of features = number of types
+# Let's check using parameter values from MRS Ch. 5 for a corpus with more than 100,000 tokens
+k <- 44
+b <- .5
+k * (Tee)^b
+M
+
+# Zipf's Law
+plot(log10(1:100), log10(topfeatures(cta_hashtag_dfm, 100)),
+     xlab = "log10(rank)", ylab = "log10(frequency)", main = "Zipf's Graph of MTA Related Hashtags")
+# Fits a linear regression to check if slope is approx -1.0
+regression <- lm(log10(topfeatures(cta_hashtag_dfm, 100)) ~ log10(1:100))
+# Adds the fitted line from regression to the plot
+abline(regression, col = "red")
+# Returns the 95% confidence intervals for the regression coefficients
+confint(regression)
+# Provides R-squared, F-test, and cofficient estimates from regression
+summary(regression)
+# Zipf's law as a feature selection tool (e.g. http://www.jmlr.org/papers/volume3/forman03a/forman03a_full.pdf)
+plot(1:100, topfeatures(cta_hashtag_dfm, 100),
+     xlab = "rank", ylab = "frequency", main = "Top 100 Words in MTA Related Hashtags")
+
+# sentiment analysis
+# Get the data directory from readtable
+positivewords <- read.table("/Users/zhengwenjie/Documents/RData/positive-words.txt")
+negativewords <- read.table("/Users/zhengwenjie/Documents/RData/negative-words.txt")
+# unlist and as.character make them bag of words
+positivewords <- as.character(unlist(positivewords))
+negativewords <- as.character(unlist(negativewords))
+
+cta_hashtags_merged$text <- tolower(cta_hashtags_merged$text)
+cta_hashtags_merged$text <- as.character(cta_hashtags_merged$text)
+
+# calculate and report the occurence of postive & negative words in the review text
+# subtract one with another to get the score
+cta_hashtags_merged$text_positive <- rowSums(dfm(cta_hashtags_merged$text, select = positivewords))
+cta_hashtags_merged$text_negative <- rowSums(dfm(cta_hashtags_merged$text, select = negativewords))
+cta_hashtags_merged$sentiment_score <- cta_hashtags_merged$text_positive - cta_hashtags_merged$text_negative
+
+# if is 0 and above, tag it positive and negative for less than 0
+cta_hashtags_merged$sentiment_attitude[cta_hashtags_merged$sentiment_score > 0] <- 'positive'
+cta_hashtags_merged$sentiment_attitude[cta_hashtags_merged$sentiment_score < 0] <- 'negative'
+cta_hashtags_merged$sentiment_attitude[cta_hashtags_merged$sentiment_score == 0] <- 'neutral'
+# number of occurence of positive & negative & the total row number in the column
+pos_number <- nrow(cta_hashtags_merged[cta_hashtags_merged$sentiment_score > 0,])
+neg_number <- nrow(cta_hashtags_merged[cta_hashtags_merged$sentiment_score < 0,])
+neu_number <- nrow(cta_hashtags_merged[cta_hashtags_merged$sentiment_score == 0,])
+
+total_rows <- nrow(cta_hashtags_merged)
+
+# calculate and report the ratio
+positive_ratio <- pos_number / total_rows
+negative_ratio <- neg_number / total_rows
+neutral_ratio <- neu_number / total_rows
+
+cat(
+  "positive_ratio: ", positive_ratio, "\n",
+  "negative_ratio:",  negative_ratio, "\n",
+  "neutral_ratio:",  neutral_ratio, "\n"
+)
+
+top_words <- textstat_frequency(dfm(cta_hashtags_merged$text), n = NULL, groups = NULL) %>% select(feature, frequency)
+head(top_words,20)
+
+# histogram
+hist(cta_hashtags_merged$sentiment_score,
+     main="Histogram of Sentiment Score", 
+     xlab="Sentiment Score",
+     ylab="Histogram Frequency Distribution",
+     border="black", 
+     col="green")
+
+
+# emoji analysis
+
+
+# nrc sentiment
+# need to figure out how nrc sentiment works
+mysentiment_cta<-get_nrc_sentiment((cta_hashtags_merged$text))
+Sentimentscores_cta<-data.frame(colSums(mysentiment_cta[,]))
+names(Sentimentscores_cta)<-"Score"
+Sentimentscores_cta<-cbind("sentiment"=rownames(Sentimentscores_cta),Sentimentscores_cta)
+rownames(Sentimentscores_cta)<-NULL
+ggplot(data=Sentimentscores_cta,aes(x=sentiment,y=Score))+geom_bar(aes(fill=sentiment),stat = "identity")+
+  theme(legend.position="none")+
+  xlab("Sentiments")+ylab("scores")+ggtitle("Sentiments of people behind the tweets on CTA hashtags")+ theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 # -----------------------------------------
 # nyct subway timeline
 # -----------------------------------------
@@ -291,3 +475,55 @@ textstat_collocations(station_delay$text) %>% arrange(-lambda) %>% slice(1:10)
 station_count_test <- data.frame(station_names,freq=rowSums(!adist(station_names,station_names,partial = T)))
 station_count_test <- station_count_test[order(-station_count_test$freq),]
 head(station_count_test)
+
+
+#ggplot(aes(x = created, y = favoriteCount), data = MTA_Hashtags_DF_510) + geom_point()
+
+
+# 
+
+
+# Summary info 
+colnames(MTA_Timeline_DF)  # column names
+colnames(NYCTSubway_Timeline_DF)
+colnames(NYCTBus_Timeline_DF)
+colnames(MTA_Hashtags_DF)  # column names
+# column names for timeline and hashtags are the same in twitter
+  # dimmension
+dim(MTA_Timeline_DF)  # data dimensions
+dim(NYCTSubway_Timeline_DF)
+dim(NYCTBus_Timeline_DF)
+dim(MTA_Hashtags_DF)
+
+sapply(MTA_Timeline_DF, class) # returns class for each variable (column)
+
+
+
+head(MTA_Timeline_DF)  # display first lines of an object
+head(polling_data, n = 10)  # same as above but specifying number of lines 
+tail(polling_data)  # display last lines of an object
+dim(MTA_Timeline_DF)  # data dimensions
+nrow(polling_data)  # number of rows
+ncol(polling_data)  # number of columns
+colnames(MTA_Timeline_DF)  # column names
+names(polling_data)  # also column names (more general command)
+rownames(polling_data) # row names
+class(polling_data)  # returns class of an R object
+sapply(polling_data, class) # returns class for each variable (column)
+str(polling_data)  # display structure of an R object (e.g. a dataframe)
+glimpse(polling_data)
+
+#userTimeline(user, n=20, maxID=NULL, sinceID=NULL, includeRts=FALSE,
+#             excludeReplies=FALSE, ...)
+#homeTimeline(n=25, maxID=NULL, sinceID=NULL, ...)
+#mentions(n=25, maxID=NULL, sinceID=NULL, ...)
+#retweetsOfMe(n=25, maxID=NULL, sinceID=NULL, ...)
+#Warning message:
+#  In statusBase(cmd, params, n, 3200, ...) :
+#  statuses/user_timeline has a cap of 3200 statuses, clipping
+
+
+
+
+
+
